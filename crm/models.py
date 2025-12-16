2ee041c6ddbd904a449edfc62fa5de08feb78b0e
Danila Melnikov <daniladm2006@mail.ru>
2025-12-16 17:21:24 +0300

    feat(clients): add Client model and admin

--- crm/models.py
+++ crm/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=120)
+    phone = models.CharField(max_length=40, blank=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.phone})" if self.phone else self.name
