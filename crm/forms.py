057137fd0d94c59faf60217a7e0d123b9a5f1ca0
mcl0udzy <kirill.ivanov7@gmail.com.com>
2025-12-17 15:49:53 +0300

    добавил нормальный ввод даты

--- crm/forms.py
+++ crm/forms.py
@@ -3,6 +3,11 @@ from .models import Reservation
 
 
 class ReservationForm(forms.ModelForm):
+    date = forms.DateField(
+        input_formats=["%d.%m.%Y"] ,
+        widget=forms.DateInput(attrs={"placeholder": "дд.мм.гггг"})
+    )
+
     class Meta:
         model = Reservation
         fields = ["client", "date", "start_time", "end_time", "table_number"]
