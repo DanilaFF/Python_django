1e6574e3e0f30bc8250712f3d48c1915624b6c37
mcl0udzy <kirill.ivanov7@gmail.com.com>
2025-12-19 13:43:39 +0300

    починил отоброжение гостей и ввод даты

--- crm/forms.py
+++ crm/forms.py
@@ -2,15 +2,23 @@ from django import forms
 from .models import Reservation
 
 
+
 class ReservationForm(forms.ModelForm):
     date = forms.DateField(
-        input_formats=["%d.%m.%Y"] ,
+        input_formats=["%d.%m.%Y"],
         widget=forms.DateInput(attrs={"placeholder": "дд.мм.гггг"})
+    )
+
+    start_time = forms.TimeField(
+        widget=forms.TimeInput(attrs={"type": "time"})
+    )
+    end_time = forms.TimeField(
+        widget=forms.TimeInput(attrs={"type": "time"})
     )
 
     class Meta:
         model = Reservation
-        fields = ["client", "date", "start_time", "end_time", "table_number"]
+        fields = ["client", "date", "start_time", "end_time", "table_number", "guests"]
 
 from .models import Client
 
