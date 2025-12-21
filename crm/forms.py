4b1ee11b368bafdcb1178c6664ffd389f8199e58
L1ghtOfQuazar <feodorgan@gmail.com>
2025-12-21 22:27:24 +0300

    updated forms for new templates

--- crm/forms.py
+++ crm/forms.py
@@ -1,34 +1,54 @@
 from django import forms
-from .models import Reservation
-
+from .models import Reservation, Client
 
 
 class ReservationForm(forms.ModelForm):
-    date = forms.DateField(
-        input_formats=["%d.%m.%Y"],
-        widget=forms.DateInput(attrs={"placeholder": "дд.мм.гггг"})
-    )
-
-    start_time = forms.TimeField(
-        widget=forms.TimeInput(attrs={"type": "time"})
-    )
-    end_time = forms.TimeField(
-        widget=forms.TimeInput(attrs={"type": "time"})
-    )
+    date = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
+    start_time = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time"}))
+    end_time = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time"}))
 
     class Meta:
         model = Reservation
-        fields = ["client", "date", "start_time", "end_time", "table_number", "guests"]
+        fields = [
+            "client",
+            "date",
+            "start_time",
+            "end_time",
+            "table_number",
+            "guests",
+            "status",
+        ]
 
-from .models import Client
+
+class PublicReservationForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, label="Имя")
+    phone = forms.CharField(
+        max_length=20,
+        label="Телефон",
+        widget=forms.TextInput(
+            attrs={
+                "type": "tel",
+                "placeholder": "+7XXXXXXXXXX",
+            }
+        ),
+    )
+
+    date = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
+    start_time = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time"}))
+    end_time = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time"}))
+
+    class Meta:
+        model = Reservation
+        fields = [
+            "date",
+            "start_time",
+            "end_time",
+            "table_number",
+            "guests",
+        ]
 
 
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ("name", "phone")
-        labels = {
-            "name": "Имя",
-            "phone": "Телефон",
-        }
-
